5388616bbe0282ccdf03611693d1d7548717b55e
Sriram-Nambiar <sriramnambiar21@gmail.com>
2026-05-31 17:22:30 +0530

    Modified appbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,9 +1,9 @@
 export function Appbar() {
   return (
-    <div style={{ display: "flex",justifyContent: "space-between", alignItems: "center", padding: 20, backgroundColor: "#af3535" }}>
+    <div style={{ display: "flex",justifyContent: "space-between", alignItems: "center", padding: 20, backgroundColor: "#1E1E1E" }}>
       <h1 style={{ margin: 0 }}>MyTube</h1>
       <div>
-        <input type="search" placeholder="Search..." />
+        <input style={{width : 500, padding: 10 , borderRadius: 20, backgroundColor:"#424242"}} type="search" placeholder="Search..." />
       </div>
       <div >
         <button onClick={() => window.location = "/uploads"} style={{ marginRight: 10 }}>Upload</button>
